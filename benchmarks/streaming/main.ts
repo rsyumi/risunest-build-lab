38899760fe23914f41d5ed305bf7a06368b6a120
rsyumi <264693545+rsyumi@users.noreply.github.com>
2026-09-20 22:11:57 +0900

    build(macos): snapshot d0084de49c679f3eaf09c7a257c36d980dd48bf1

--- benchmarks/streaming/main.ts
+++ benchmarks/streaming/main.ts
@@ -1,7 +1,11 @@
 import "../../src/ts/polyfill";
 import "core-js/actual";
 import "katex/dist/katex.min.css";
-import "../../src/ts/storage/deviceSettingsStartup";
+import { isTauri } from "../../src/ts/platform";
+import { getDeviceSettings } from "../../src/ts/storage/deviceSettings";
+import { initializeDeviceMarkers } from "../../src/ts/storage/deviceMarkers";
 import { startStreamingSmoke } from "./fixture";
 
+if (isTauri) await initializeDeviceMarkers();
+getDeviceSettings();
 startStreamingSmoke();
